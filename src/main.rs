c248c8ca5ed0b740db0e96696a3b1e38c35b86d4
rodey <rodeyluo@gmail.com>
2021-04-12 16:45:52 +0800

    add guess parse match

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,10 @@ fn main() {
 
         let mut guess = String::new();
         io::stdin().read_line(&mut guess).expect("读取失败");
-        let guess: u32 = guess.trim().parse().expect("不是数字，转换失败");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
         // println!("当前数字为: {}", guess);
 

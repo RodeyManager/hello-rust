b918bdebc20e80982f15dc085be77c0c00e32025
rodey <rodeyluo@gmail.com>
2021-04-12 16:35:17 +0800

    add match and Ordering

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,24 @@
 use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("猜数字！");
 
-    let secret_number = rand::thread_rng().gen::<u32>();
+    let secret_number = rand::thread_rng().gen_range(1..101);
 
     println!("密码数字: {}", secret_number);
     println!("请输入一个数字: ");
 
     let mut guess = String::new();
     io::stdin().read_line(&mut guess).expect("读取失败");
+    let guess: u32 = guess.trim().parse().expect("不是数字，转换失败");
 
     println!("当前数字为: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too Big!"),
+        Ordering::Equal => println!("You Win!"),
+    }
 }

68f80ae7fe20d96c392647a6167235916968b772
rodey <rodeyluo@gmail.com>
2021-04-26 22:58:15 +0800

    HashMap、String & &str

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-fn main() {
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::{self, Read, Write};
+
+fn main() -> io::Result<()> {
     // ---- 常见集合 ----
 
     // vector 允许我们一个挨着一个地储存一系列数量可变的值
@@ -161,4 +165,135 @@ fn main() {
     for b in "你好，Rust".bytes() {
         println!("{}", b);
     }
+
+    // 字符串并不简单
+
+    // 哈希 map 储存键值对（要使用 HashMap必须先 use std::collections::HashMao）
+    let mut map = HashMap::new();
+    map.insert(String::from("Red"), 89);
+    map.insert(String::from("Yellow"), 80);
+    map.insert(String::from("blue"), 90);
+    println!("map: {:#?}", map);
+
+    // 类似于 vector，哈希 map 是同质的：所有的键必须是相同类型，值也必须都是相同类型。
+    // HashMap<T, U>
+    let teams = vec![
+        String::from("Red"),
+        String::from("Yellow"),
+        String::from("Blue"),
+    ];
+    let coins = vec![90, 89, 100];
+    let map: HashMap<_, _> = teams.iter().zip(coins.iter()).collect();
+    // TODO: 转换后为什么顺序改变了???? { "Yellow": 89, "Blue": 100, "Red": 90 }
+    println!("map: {:#?}", map);
+
+    // 哈希 map 和所有权
+    let first_name = String::from("Rodey");
+    let last_name = String::from("Luo");
+    let mut name = HashMap::new();
+    name.insert(first_name, last_name);
+    println!("name: {:?}", name);
+    // 由于String拥有所有权，而map.insert后将所有权移动到insert中，所以first_name,last_name此后不再有效
+    // println!("my name: {} {}", first_name, last_name);
+
+    // 访问哈希 map 中的值
+    let mut map = HashMap::new();
+    map.insert(String::from("Red"), 255.0);
+    map.insert(String::from("Green"), 55.0);
+    map.insert(String::from("Blue"), 155.0);
+    map.insert(String::from("Alpha"), 0.6);
+    let red = map.get("Red"); // => Option
+    println!("Red coins: {:?}", red);
+
+    for (key, value) in &map {
+        println!("{}: {}", key, value);
+    }
+
+    // 更新
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 78);
+    scores.insert(String::from("Blue"), 98);
+    println!("scores: {:?}", scores);
+
+    // 只在键没有对应值时插入
+    scores.entry(String::from("Red")).or_insert(98);
+    scores.entry(String::from("Blue")).or_insert(100);
+    println!("scores: {:?}", scores);
+
+    let s = "red greeb blue alpha red";
+    let mut map = HashMap::new();
+
+    for word in s.split_whitespace() {
+        // ****以下为重点：
+        // or_insert 方法事实上会返回这个键的值的一个可变引用（&mut V）。这里我们将这个可变引用储存在 count 变量中，
+        // 所以为了赋值必须首先使用星号（*）解引用 count。
+        // 这个可变引用在 for 循环的结尾离开作用域，这样所有这些改变都是安全的并符合借用规则。
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+    // map: {"greeb": 1, "alpha": 1, "red": 2, "blue": 1}
+    println!("map: {:?}", map);
+
+    // 练习----------
+    // 1、给定一系列数字，使用 vector 并返回这个列表的平均数（mean, average）、中位数（排列数组后位于中间的值）和众数（mode，出现次数最多的值；这里哈希函数会很有帮助）。
+    let numbers = vec![2, 1, 3, 5, 67, 32, 23, 5, 32];
+    let mut sum = 0;
+    let mut string = "".to_string();
+    let len = numbers.len();
+    for num in numbers.iter() {
+        sum += num;
+        string.push_str(&num.to_string());
+    }
+    let average = sum / len;
+    println!("sum: {}", sum);
+    println!("average: {}", average);
+    println!("string: {}", string);
+
+    let strings: Vec<&str> = string.split("").collect();
+    let mut map = HashMap::new();
+    for num in strings.iter() {
+        // println!("num: {}", num);
+        if !num.is_empty() {
+            let count = map.entry(num).or_insert(0);
+            *count += 1;
+        }
+    }
+    println!("map: {:?}", map);
+
+    // 2、将字符串转换为 Pig Latin，也就是每一个单词的第一个辅音字母被移动到单词的结尾并增加 “ay”，所以 “first” 会变成 “irst-fay”。
+    // 元音字母开头的单词则在结尾增加 “hay”（“apple” 会变成 “apple-hay”）。牢记 UTF-8 编码！
+    let consonant: Vec<&str> = "b,c,d,f,g,h,j,k,l,m,n,p,q,r,s,t,v,w,x,y,z"
+        .split(",")
+        .collect();
+    let metes: Vec<&str> = "a,e,i,o,u".split(",").collect();
+    let words = "first last middle big apple out is good".split_whitespace();
+    let mut map = HashMap::new();
+
+    for word in words {
+        let word = Some(word).unwrap();
+
+        // 辅音字母
+        for letter in consonant.iter() {
+            if word.starts_with(letter) {
+                let text = map.entry(word).or_insert(String::from(""));
+                *text = format!("{}-{}ay", word.strip_prefix(letter).unwrap(), letter);
+            }
+        }
+
+        // 元音字母
+        for letter in metes.iter() {
+            if word.starts_with(letter) {
+                let text = map.entry(word).or_insert(String::from(""));
+                *text = format!("{}-hay", word);
+            }
+        }
+    }
+
+    println!("map: {:?}", map);
+
+    // 3、使用哈希 map 和 vector，创建一个文本接口来允许用户向公司的部门中增加员工的名字。
+    // 例如，“Add Sally to Engineering” 或 “Add Amir to Sales”。
+    // 接着让用户获取一个部门的所有员工的列表，或者公司每个部门的所有员工按照字典序排列的列表。
+
+    Ok(())
 }
